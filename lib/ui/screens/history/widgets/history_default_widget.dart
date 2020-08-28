4307d16b270aadd9f4622e032676d676d381a562
shyboy <www.maharjandinesh@gmail.com>
2020-08-28 18:58:05 +0545

    History default page is done

---
+++ lib/ui/screens/history/widgets/history_default_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hero_premier/ui/shared/asset_paths.dart';
+import 'package:hero_premier/ui/shared/text_styles.dart';
+
+class HistoryDefaultWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            AssetPaths.IC_NO_HISTORY,
+            width: 150,
+            height: 150,
+          ),
+          SizedBox(
+            height: 24.0,
+          ),
+          Text(
+            "No game history found yet !",
+            style: TextStyles.Heading3.copyWith(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 4.0,
+          ),
+          Text(
+            "You haven't played a game for now",
+            style: TextStyles.Subtitle2.copyWith(
+              color: Colors.black.withOpacity(0.54),
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

a43a458f8d4627dccef29195da15be15adea7da2
shyboy <www.maharjandinesh@gmail.com>
2020-09-01 23:27:01 +0545

    added reward desing

--- lib/ui/screens/winner/winner_screen.dart
+++ lib/ui/screens/winner/winner_screen.dart
@@ -3,7 +3,9 @@ import 'package:hero_premier/core/models/winner_result.dart';
 import 'package:hero_premier/core/view_models/winner_view_model.dart';
 import 'package:hero_premier/router.dart';
 import 'package:hero_premier/ui/base_widget.dart';
+import 'package:hero_premier/ui/screens/leaderboard/widgets/reward_card.dart';
 import 'package:hero_premier/ui/screens/winner/widgets/winner_card.dart';
+import 'package:hero_premier/ui/shared/asset_paths.dart';
 import 'package:hero_premier/ui/shared/text_styles.dart';
 import 'package:hero_premier/ui/widgets/error_card.dart';
 import 'package:provider/provider.dart';
@@ -20,7 +22,7 @@ class _WinnerScreenState extends State<WinnerScreen>
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
+    _tabController = TabController(length: 4, vsync: this);
   }
 
   @override
@@ -69,6 +71,7 @@ class _WinnerScreenState extends State<WinnerScreen>
                         currentSeasonWinners(winners, model),
                         Icon(Icons.settings),
                         Icon(Icons.settings),
+                        rewardWidget(),
                       ],
                     ),
                   ),
@@ -100,6 +103,9 @@ class _WinnerScreenState extends State<WinnerScreen>
         Tab(
           text: "2019/2018",
         ),
+        Tab(
+          text: "Reward",
+        ),
       ],
       controller: _tabController,
     );
@@ -124,6 +130,48 @@ class _WinnerScreenState extends State<WinnerScreen>
           onTap: () => model.selectWinner(winner.weekInfo.winnerInfo.id),
         );
       }).toList(),
+    );
+  }
+
+  Widget rewardWidget() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          RewardCardWidget(
+            position: "1st",
+            backgroundColor: Color(0xFFFFDE46),
+            backgroundImgUrl: AssetPaths.IC_BACKGROUND_REWARD,
+            icon: AssetPaths.IC_FIRST_MEDAL,
+            title: "50K Cash prize for the winner",
+            descripion:
+                "Hello, winner first of leaderboard get 50,000 cash in hand gift",
+            fontColor: Color(0xFF121212),
+            buttonColor: Color(0xFFCEB648),
+          ),
+          RewardCardWidget(
+            position: "2nd",
+            backgroundColor: Color(0xFF75C5F2),
+            backgroundImgUrl: AssetPaths.IC_BACKGROUND_REWARD,
+            icon: AssetPaths.IC_SECOND_MEDAL,
+            title: "30K Cash prize for the winner",
+            descripion:
+            "Hello, winner first of leaderboard get 30,000 cash in hand gift",
+            fontColor: Color(0xFF121212),
+            buttonColor: Color(0xFFA7D2D7),
+          ),
+          RewardCardWidget(
+            position: "3rd",
+            backgroundColor: Color(0xFFAC862F),
+            backgroundImgUrl: AssetPaths.IC_BACKGROUND_REWARD,
+            icon: AssetPaths.IC_THIRD_MEDAL,
+            title: "20K Cash prize for the winner",
+            descripion:
+            "Hello, winner first of leaderboard get 20,000 cash in hand gift",
+            fontColor: Color(0xFFFFFFFF),
+            buttonColor: Color(0xFFC9A85C),
+          )
+        ],
+      ),
     );
   }
 }
